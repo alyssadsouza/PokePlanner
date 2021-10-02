c7b1b6701206b70751756d554e1d3d7ac7203140
alyssadsouza <alyssa.dsouza63@gmail.com>
2021-10-02 17:42:51 -0400

    Add login and register page

--- pokeplanner/models.py
+++ pokeplanner/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
--- pokeplanner/views.py
+++ pokeplanner/views.py
@@ -1,7 +1,67 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.urls import reverse
+#from .models import User
+from django.contrib.auth.models import User
 
  # Create your views here.
 
+@login_required(login_url="login")
 def index(request):
     return render(request, "index.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            return render(request, "login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "login.html")
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect("index")
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return redirect("index")
+    else:
+        return render(request, "register.html")
